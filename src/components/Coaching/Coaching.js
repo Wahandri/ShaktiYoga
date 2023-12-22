db9b039f56815721d4721c65d6880727273f2dc4
Tu Nombre <wahandri@wahandri-VirtualBox>
2023-12-22 17:15:07 +0100

    update titles

--- src/components/Coaching/Coaching.js
+++ src/components/Coaching/Coaching.js
@@ -1,42 +1,67 @@
-import React from 'react';
+import React from "react";
 import "./Coaching.css";
 import imgPositive from "../../images/positive.jpg";
 import imgTeam from "../../images/emocionalcoaching.jpg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Coaching() {
   return (
-    <div className='boxCoaching flex column center'>
-        <h1 className='title'><span class="blueColor">Coaching</span> Personalizado</h1>
+    <div className="boxCoaching flex column center">
+      <h1 className="title">
+        Terapia<span class="blueColor"> breve</span> estratégica
+      </h1>
 
-        {/* Sección 1: Intervención Emocional */}
-        <div className='coaching1 flex shadowLight responsiveCoaching'>
-            <div className='boxCoachingImg'>
-                <img className='imgCoaching margin' src={imgPositive} alt='Intervención Emocional' />
-            </div>
-            <div className='descriptionCoaching'>
-                <h1 className='titleSmall'>Intervención Emocional Estratégica</h1>
-                <p>
-                    Descubre conmigo, <span className='blueColor'>Simona Zappoli</span>, la eficacia de la intervención emocional estratégica. Este proceso te guiará desde tu presente hacia tu pasado, desbloqueando emociones y traumas que pueden estar impidiéndote avanzar. A través de este método eficaz y corto, aprenderás a gestionar tu vida con nuevas herramientas, liberándote de ataduras para siempre.
-                </p>
-            </div>
+      {/* Sección 1: Intervención Emocional */}
+      <div className="coaching1 flex shadowLight responsiveCoaching">
+        <div className="boxCoachingImg">
+          <img
+            className="imgCoaching margin"
+            src={imgPositive}
+            alt="Intervención Emocional"
+          />
         </div>
+        <div className="descriptionCoaching">
+          <h1 className="titleSmall">Intervención Emocional Estratégica</h1>
+          <p>
+            Descubre conmigo, <span className="blueColor">Simona Zappoli</span>,
+            la eficacia de la intervención emocional estratégica. Este proceso
+            te guiará desde tu presente hacia tu pasado, desbloqueando emociones
+            y traumas que pueden estar impidiéndote avanzar. A través de este
+            método eficaz y corto, aprenderás a gestionar tu vida con nuevas
+            herramientas, liberándote de ataduras para siempre.
+          </p>
+        </div>
+      </div>
 
-        {/* Sección 2: Coaching */}
-        <div className='coaching2 flex paddingTop shadowLight responsiveCoaching revers'>
-            <div className='descriptionCoaching'>
-                <h1 className='titleSmall'>Coaching Emocional: Crea tu Futuro Brillante</h1>
-                <p>
-                    Sumérgete conmigo en el proceso de Coaching Emocional. Desde tu presente hacia tu futuro, exploraremos juntos la creación de una vida llena y brillante. Este enfoque te permitirá encontrar tu potencial y convertirte en el dueño de tu vida. Los procesos son eficaces, cortos y no te hacen dependiente del Coach, permitiéndote aprender a gestionar tu vida con nuevas herramientas. ¡Deja que el Coaching Emocional sea tu guía hacia una vida plena y satisfactoria!
-                </p>
-            </div>
-            <div className='boxCoachingImg'>
-                <img className='imgCoaching margin' src={imgTeam} alt='Coaching Emocional' />
-            </div>
+      {/* Sección 2: Coaching */}
+      <div className="coaching2 flex paddingTop shadowLight responsiveCoaching revers">
+        <div className="descriptionCoaching">
+          <h1 className="titleSmall">
+            Coaching de Bienestar:
+            <br />
+            "Crea tu futuro brillante"
+          </h1>
+          <p>
+            Sumérgete conmigo en el proceso de Coaching Emocional. Desde tu
+            presente hacia tu futuro, exploraremos juntos la creación de una
+            vida llena y brillante. Este enfoque te permitirá encontrar tu
+            potencial y convertirte en el dueño de tu vida. Los procesos son
+            eficaces, cortos y no te hacen dependiente del Coach, permitiéndote
+            aprender a gestionar tu vida con nuevas herramientas. ¡Deja que el
+            Coaching Emocional sea tu guía hacia una vida plena y satisfactoria!
+          </p>
         </div>
-        <Link to='/coaching'>
-            <button className='btn'>Saber mas</button>
-        </Link> 
+        <div className="boxCoachingImg">
+          <img
+            className="imgCoaching margin"
+            src={imgTeam}
+            alt="Coaching Emocional"
+          />
+        </div>
+      </div>
+      <Link to="/coaching">
+        <button className="btn">Saber mas</button>
+      </Link>
     </div>
   );
 }
--- src/components/Start/Start.js
+++ src/components/Start/Start.js
@@ -1,34 +1,39 @@
-import React from 'react'
-import "./Start.css"
-import logoShakti from "../../images/shakti.png"
-import Coaching from '../Coaching/Coaching'
-import Meditation from '../Meditation/Meditation'
-import Massage from '../Massage/Massage'
-import Yoga from "../Yoga/Yoga.js"
-import Nutrition from '../Nutrition/Nutrition.js'
+import React from "react";
+import "./Start.css";
+import logoShakti from "../../images/shakti.png";
+import Coaching from "../Coaching/Coaching";
+import Meditation from "../Meditation/Meditation";
+import Massage from "../Massage/Massage";
+import Yoga from "../Yoga/Yoga.js";
+import Nutrition from "../Nutrition/Nutrition.js";
 
 export default function Start() {
-
   return (
-    <div className='boxStart flex column center'>
-        <div className='startHeader'>
-            <img className='logoShakti' src={logoShakti} alt=""/>
-            <div className='margin'>
-                <div className='flex center'>
-                    <h1 className='title'>Bienvenido a <span className='pinkColor'>Shakti</span></h1>
-                </div>
-                <p className='textSmall'>
-                    Descubre el poder transformador de tus emociones con nuestro <span className='pinkColor'>coaching personalizado</span>.<br/> 
-                    En <span className='pinkColor'>Shakti</span>, te acompañamos en un viaje hacia la autenticidad y el bienestar emocional.<br/> 
-                    Encuentra claridad, supera desafíos y construye relaciones más significativas contigo mismo y con los demás.
-                </p>
-            </div>
+    <div className="boxStart flex column center">
+      <div className="startHeader">
+        <img className="logoShakti" src={logoShakti} alt="" />
+        <div className="margin">
+          <div className="flex center">
+            <h1 className="title">
+              Bienvenido a <span className="pinkColor">Shakti</span>
+            </h1>
+          </div>
+          <p className="textSmall">
+            Descubre el poder transformador de tus emociones con nuestro{" "}
+            <span className="pinkColor">procesos personalizado</span>.<br />
+            En <span className="pinkColor">Shakti</span>, te acompañamos en un
+            viaje hacia la autenticidad y el bienestar emocional.
+            <br />
+            Encuentra claridad, supera desafíos y construye relaciones más
+            significativas contigo mismo y con los demás.
+          </p>
         </div>
-        <Coaching />
-        <Meditation />
-        <Yoga />
-        <Massage />
-        <Nutrition />
+      </div>
+      <Coaching />
+      <Meditation />
+      <Yoga />
+      <Massage />
+      <Nutrition />
     </div>
-  )
+  );
 }
