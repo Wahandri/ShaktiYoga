4eab205ce69647c5328b4667d083e6ccca5e9c07
Tu Nombre <wahandri@wahandri-VirtualBox>
2023-12-31 03:14:49 +0100

    updated css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Start from './components/Start/Start';
-import Header from './components/Header/Header';
-import PageCoaching from './components/Coaching/PageCoaching';
-import PageMeditation from './components/Meditation/PageMeditation';
-import PageMassage from './components/Massage/PageMassage';
-import PageYoga from './components/Yoga/PageYoga';
-import PageNutrition from './components/Nutrition/PageNutrition';
-import PageContact from './components/PageContact/PageContact';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Start from "./components/Start/Start";
+import Header from "./components/Header/Header";
+import PageCoaching from "./components/Coaching/PageCoaching";
+import PageMeditation from "./components/Meditation/PageMeditation";
+import PageMassage from "./components/Massage/PageMassage";
+import PageYoga from "./components/Yoga/PageYoga";
+import PageNutrition from "./components/Nutrition/PageNutrition";
+import PageContact from "./components/PageContact/PageContact";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
+        <div className="marginHeader"> </div>
         <Routes>
           <Route path="/" element={<Start />} />
           <Route path="/coaching" element={<PageCoaching />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,88 @@
-import React from 'react'
-import "./Header.css"
-import logoShakti from "../../images/shaktiSmall.png"
-import instagram from "../../images/instagram.png"
-import facebook from "../../images/facebook.png"
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./Header.css";
+import logoShakti from "../../images/shaktiSmall.png";
+import facebook from "../../images/facebook.png";
+import { Link } from "react-router-dom";
+import shop from "../../images/bolsa-de-la-compra.png";
+import menu from "../../images/menu.png";
 
 export default function Header() {
-  return (
-    <div className='boxHeader'>
-        <Link to="/" className='imgLogo'><img width="90px" src={logoShakti} alt=''/></Link>
-        <div className='navBar'>
-          <Link className='ruteNavBar flex center' to="/coaching">Coaching</Link>
-          <Link className='ruteNavBar flex center' to="/meditation">Meditacion</Link>
-          <Link className='ruteNavBar flex center' to="/massage">Masajes</Link>
-          <Link className='ruteNavBar flex center' to="/yoga">Yoga</Link>
-          <Link className='ruteNavBar flex center' to="/nutrition">Nutricion</Link>
-          <Link className='ruteNavBar flex center' to="/contact">Contáctanos</Link>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-        </div>
-        <div className='socialNetworkIcons'>
-          <a href="https://www.google.es" target="_blank" rel="noopener noreferrer">
-            <img className='iconSocial' src={instagram} alt=""/>
-          </a>
-          <img className='iconSocial' src={facebook} alt=""/>
-        </div>
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={`boxHeader ${menuOpen ? "menuOpen" : ""}`}>
+      <Link to="/" onClick={closeMenu} className="imgLogo">
+        <img width="90px" src={logoShakti} title="Inicio" alt="" />
+      </Link>
+      <div className="menuButton" onClick={toggleMenu}>
+        <img src={menu} title="Menu" width="35px" alt="menu" />
+      </div>
+      <ul className="navBar">
+        <Link
+          onClick={closeMenu}
+          className="ruteNavBar flex center"
+          to="/coaching"
+        >
+          Coaching
+        </Link>
+        <Link
+          onClick={closeMenu}
+          className="ruteNavBar flex center"
+          to="/meditation"
+        >
+          Meditacion
+        </Link>
+        <Link
+          onClick={closeMenu}
+          className="ruteNavBar flex center"
+          to="/massage"
+        >
+          Masajes
+        </Link>
+        <Link onClick={closeMenu} className="ruteNavBar flex center" to="/yoga">
+          Yoga
+        </Link>
+        <Link
+          onClick={closeMenu}
+          className="ruteNavBar flex center"
+          to="/nutrition"
+        >
+          Nutricion
+        </Link>
+        <Link
+          onClick={closeMenu}
+          className="ruteNavBar flex center"
+          to="/contact"
+        >
+          Contáctanos
+        </Link>
+      </ul>
+      <div className="socialNetworkIcons">
+        <a
+          href="https://shop.foreverliving.it/zappoli-simona-340000606427.html"
+          target="_blank"
+          title="Tienda"
+          rel="noopener noreferrer"
+        >
+          <img className="iconSocial" src={shop} alt="" />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100064173574424"
+          target="_blank"
+          title="Facebook"
+          rel="noopener noreferrer"
+        >
+          <img className="iconSocial" src={facebook} alt="" />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
