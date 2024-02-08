cc6441069b63bf890568a4845b1cc5c474aee344
Wahandri <wahandricode@gmail.com>
2024-02-08 19:38:30 +0100

    update number

--- src/components/FloatButton/FloatButton.js
+++ src/components/FloatButton/FloatButton.js
@@ -5,7 +5,7 @@ import whatsapp from "../../images/whatsapp.png";
 export default function FloatButton() {
   return (
     <div className='boxFloatButton'>
-      <a className='floatButton' target="_blank" without rel="noopener noreferrer" href="https://wa.me/+34629612538?text=%F0%9F%91%8B%20Hola%20Simona,%20estoy%20interesado%20en%20obtener%20más%20información%20sobre%20tus%20servicios.">
+      <a className='floatButton' target="_blank" without rel="noopener noreferrer" href="https://wa.me/+34644808264git?text=%F0%9F%91%8B%20Hola%20Simona,%20estoy%20interesado%20en%20obtener%20más%20información%20sobre%20tus%20servicios.">
         <img lassName='floatingIcon' src={whatsapp} alt="whatsapp" className="floatButton" />
       </a>
     </div>
